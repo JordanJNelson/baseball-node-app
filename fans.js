71188a3cb4e597bfbd45b16ea66a6d39b6fded2f
JordanJNelson <litcircleapp@gmail.com>
2023-10-17 19:55:40 -0700

    updated for postman

--- fans.js
+++ fans.js
@@ -1,9 +1,1 @@
-function goTeam(team) {
-  return `LET'S GO ${team.toUpperCase()}!`;
-}
-
-
-module.exports = {
-  letsGoTeam,
-  teamSucks,
-}
+// fans
--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const { goTeam } = require('./fandom.js');
+const {} = require('./fans')
 const fs = require('fs');
 
-// Set Up Routes
-// Core Routes
+app.get('/', (req, res) =>{
+  res.json({message:" Hello from my Home Route"})
+});
+
 app.get('/read', (req, res) => {
   let element = req.query.something;
   fs.readFile(`${element}.txt`, 'utf8', (error, data) => {
@@ -20,31 +22,26 @@ app.get('/read', (req, res) => {
   })
 })
 
-// Local Routes
-app.get('/letsgoteam/:team', (req, res) => {
-  let answer = letsGoTeam(String(req.params.team));
-  return res.json({
-    answer: answer
-  });
-});
-
-app.get('/teamsucks/:team', (req, res) => {
-  let answer = teamSucks(String(req.params.team));
-  return res.json({
-    answer: answer
-  });
-});
-
-// Third-Party Routes
-app.get('/', (req, res) => {
-  return res.json({
-    message: 'Welcome to the World Marbles Championship App'
-  });
-})
-
+// app.get('/letsgoteam/:team', (req, res) => {
+//   let answer = letsGoTeam(String(req.params.team));
+//   return res.json({
+//     answer: answer
+//   });
+// });
+// app.get('/teamsucks/:team', (req, res) => {
+//   let answer = teamSucks(String(req.params.team));
+//   return res.json({
+//     answer: answer
+//   });
+// });
+// // Third-Party Routes
+// app.get('/', (req, res) => {
+//   return res.json({
+//     message: 'Welcome to the  Championship '
+//   });
+// })
 // Port setup and listener
 const PORT = process.env.PORT || 8000;
-
 app.listen(PORT, () => {
   console.log('Server is running on PORT', PORT);
 })
